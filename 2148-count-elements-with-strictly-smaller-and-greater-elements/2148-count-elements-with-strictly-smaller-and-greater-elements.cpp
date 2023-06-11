21ac733680391c59fb2e5080b162a572aa67280d
AMAN RAJ SHARMA <119055650+DestructorAMAN@users.noreply.github.com>
2023-06-11 12:50:17 +0530

    Time: 6 ms (30.99%), Space: 9 MB (97.81%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     int countElements(vector<int>& nums) {
-       int n = nums.size();
-        int count=0;
-       for(int i=0;i<n;i++){
-           int flag=0;
-           int ptr= nums[i];
-           for(int j=0;j<n;j++){
-               if(ptr>nums[j])
-                   {flag++;
-                   break;}
-           }
-           for(int j=0;j<n;j++){
-               if(ptr<nums[j])
-                   {flag++;
-                   break;}
-           }
-           if(flag==2) count++;
-       }
-        return count;
-    }
+      sort(nums.begin(), nums.end());
+        
+        short start_index = 0;
+        short end_index = nums.size() - 1;
+        
+         if (nums.size() <= 2) {
+            return 0;
+        }
+         for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                start_index += 1;
+            } else {
+                break;
+            }
+        }
+         if (start_index + 1 == nums.size()) {
+            return 0;
+        }
+          for (int i = nums.size() - 1; i >= start_index + 1; i--) {
+            if (nums[i] == nums[i - 1]) {
+                end_index -= 1;
+            } else {
+                break;
+            }
+        }        
+         if (start_index == 0 && end_index == nums.size() - 1) {
+            return nums.size() - 2;
+        }
+         else if (start_index == 0) {
+            return end_index - 1;
+        }
+         else {
+            return end_index - start_index - 1;
+        }
+     return 0;}
 };
