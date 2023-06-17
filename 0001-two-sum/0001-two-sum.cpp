346540c85e3b19f8dc10c1af64a4127fa9343741
AMAN RAJ SHARMA <119055650+DestructorAMAN@users.noreply.github.com>
2023-06-17 16:05:56 +0530

    Time: 15 ms (64.30%), Space: 12.4 MB (5.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,13 +5,11 @@ public:
     map<int,int> mpp;
         int n=nums.size();
     for(int i=0;i<n;i++){
-        int a=nums[i];
-        int more=target-a;
-        if(mpp.find(more)!=mpp.end()){
-           ans.push_back(mpp[more]);
+        if(mpp.find(target-nums[i])!=mpp.end()){
+           ans.push_back(mpp[target-nums[i]]);
             ans.push_back(i);
         }
-        mpp[a]=i;
+        mpp[nums[i]]=i;
     }
     return ans;
     }
